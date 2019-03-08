3d931b86f1c685dec4b1c4291fddc04d937e14ad
Emilien.Maintenay <adadabide2@gmail.com>
2019-03-08 18:26:28 +0100

    Ajouts graphiques + démarrage langages

--- wordpress/wp-content/themes/BagadLanderne/header.php
+++ wordpress/wp-content/themes/BagadLanderne/header.php
@@ -10,7 +10,7 @@
         <!– CSS de Bootstrap –>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
         <!– Ajout d’une nouvelle feuille de style qui sera spécifique à notre thème –>
-        <link href="<?php bloginfo(‘template_directory’);?>/blog.css" rel="stylesheet">
+        <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
         <!– HTML5 shim et Respond.js pour supporter les éléments HTML5 pour les versions plus anciennes que Internet Explorer 9 –>
         <!–[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
